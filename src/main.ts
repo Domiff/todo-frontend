ac29b41345b3436533888cc7467aeda2fb3fc122
dmitriy levykin <dmlegasy@gmail.com>
2026-03-21 21:24:48 +0300

    Include pinia in app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from '@/App.vue'
+import { createApp } from "vue"
+import router from "./router"
+import App from "@/App.vue"
+import "vuetify/styles"
+import { createVuetify } from "vuetify"
 
 export const app = createApp(App)
+const vuetify = createVuetify()
+
+app.use(vuetify)
 app.use(router)
 
-app.mount('#app')
+app.mount("#app")
