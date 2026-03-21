70d26b663f8408d1c55de9d8dced9abd2f85bb9a
dmitriy levykin <dmlegasy@gmail.com>
2026-03-21 21:25:42 +0300

    Add home, register and login routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import type { Router } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
+import type { Router } from "vue-router"
+import Register from "@/views/RegisterView.vue"
+import HomeView from "@/views/HomeView.vue"
+import Login from "@/views/LoginView.vue"
+
+const routes = [
+    { path: "/", component: HomeView },
+    { path: "/register", component: Register },
+    { path: "/login", component: Login },
+]
 
 const router: Router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes,
 })
 
 export default router
