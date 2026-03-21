50dc05d0b9c2a968db1d914805bda06f735a9ece
dmitriy levykin <dmlegasy@gmail.com>
2026-03-21 21:32:14 +0300

    Add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-app-bar color="primary" dark>
+        <v-toolbar-title>ToDo App</v-toolbar-title>
+        <v-spacer />
+        <v-btn to="/" variant="text"> Home </v-btn>
+        <v-btn to="/register" variant="text"> Register </v-btn>
+        <v-btn to="/login" variant="text"> Login </v-btn>
+    </v-app-bar>
+</template>
