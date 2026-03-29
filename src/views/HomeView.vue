c23c55e393d4556678e57aef97367e46e833d8da
dmitriy levykin <dmlegasy@gmail.com>
2026-03-29 22:47:44 +0300

    feat(ui): remove navbar from home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,5 @@
-<script setup lang="ts">
-    import Navbar from "@/components/Navbar.vue"
-</script>
-
 <template>
     <v-app>
-        <Navbar />
         <v-main>
             <v-container class="text-center mt-10">
                 <h1>Welcome to ToDo!</h1>
