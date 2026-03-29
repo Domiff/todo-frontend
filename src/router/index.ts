5892c70a6b7bdaab92b0f80e2cd5d03aae50ec2b
dmitriy levykin <dmlegasy@gmail.com>
2026-03-29 22:50:05 +0300

    feat: add todo routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router"
 
 import { authRoutes } from "@/modules/auth/routes"
+import { todoRoutes } from "@/modules/todo/routes"
 import HomeView from "@/views/HomeView.vue"
 
-const routes = [{ path: "/", component: HomeView }, ...authRoutes]
+const routes = [{ path: "/", component: HomeView }, ...authRoutes, ...todoRoutes]
 
 export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
